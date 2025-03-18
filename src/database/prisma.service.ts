8541091f986a48d36e7e1d785fed983af4aeaf3c
kkn1125 <xpfjrnseks1@naver.com>
2025-03-18 13:25:52 +0900

    🛠️ fix: Prisma class generator 커스터마이징 추가 + Prisma Extension 추가

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,8 +1,41 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+export type ExtensionType = ReturnType<typeof prismaExtension>;
+
+const prismaExtension = (client: PrismaClient) => {
+  async function softDelete<
+    Model,
+    Args extends Prisma.Args<Model, 'delete' | 'deleteMany'>,
+  >(this: Model, args?: Args) {
+    const argsCopy = args ?? {};
+    Object.assign(argsCopy, {
+      data: { deletedAt: new Date() },
+    });
+    const context = Prisma.getExtensionContext(this) as any;
+    const item = await context.update(argsCopy);
+    return { item };
+  }
+
+  const extension = Prisma.defineExtension({
+    name: 'customExtension',
+    model: {
+      $allModels: { softDelete },
+    },
+  });
+
+  return client.$extends(extension);
+};
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  _client!: ExtensionType;
+
+  get client() {
+    if (!this._client) this._client = prismaExtension(this);
+    return this._client;
+  }
+
   constructor() {
     super({
       log: ['query'],
